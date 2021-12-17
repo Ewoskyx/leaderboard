d9e824ae3a71c206b0ac6d18ab24abd081b4c62f
Ewoskyx <ewoskyx@gmail.com>
2021-12-17 05:13:49 +0300

    Add sort functions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const nameVal = document.getElementById('nameInput');
 const scoreVal = document.getElementById('scoreInput');
 const refresh = document.getElementById('refresh-btn');
 const scoreBoard = document.getElementById('scoreBoard');
+const sort = document.getElementById('sort-selector');
 
+// Add Score
 const form = document.querySelector('.form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -20,17 +22,87 @@ form.addEventListener('submit', (e) => {
   scoreVal.value = '';
 });
 
-const getScores = () => {
+// Sort Functions
+const getScoresDescending = () => {
   getScore().then((data) => {
     const list = data.result;
+    const sortedList = list.sort((s1, s2) => (s2.score - s1.score));
     scoreBoard.innerHTML = '';
-    list.forEach((item) => {
+    sortedList.forEach((item) => {
       renderBoardItem(item);
     });
   });
 };
+
+const getScoresAscending = () => {
+  getScore().then((data) => {
+    const list = data.result;
+    const sortedList = list.sort((s1, s2) => (s1.score - s2.score));
+    scoreBoard.innerHTML = '';
+    sortedList.forEach((item) => {
+      renderBoardItem(item);
+    });
+  });
+};
+
+const getNamesAscending = () => {
+  getScore().then((data) => {
+    const list = data.result;
+    const sortedList = list.sort((s1, s2) => {
+      const userA = s1.user.toUpperCase();
+      const userB = s2.user.toUpperCase();
+      if (userA < userB) {
+        return -1;
+      }
+      if (userA > userB) {
+        return 1;
+      }
+      return 0;
+    });
+    scoreBoard.innerHTML = '';
+    sortedList.forEach((item) => {
+      renderBoardItem(item);
+    });
+  });
+};
+
+const getNamesDescending = () => {
+  getScore().then((data) => {
+    const list = data.result;
+    const sortedList = list.sort((s1, s2) => {
+      const userA = s1.user.toUpperCase();
+      const userB = s2.user.toUpperCase();
+      if (userA < userB) {
+        return 1;
+      }
+      if (userA > userB) {
+        return -1;
+      }
+      return 0;
+    });
+    scoreBoard.innerHTML = '';
+    sortedList.forEach((item) => {
+      renderBoardItem(item);
+    });
+  });
+};
+
+// Refresh event
 refresh.addEventListener('click', () => {
-  getScores();
+  getScoresDescending();
   nameVal.value = '';
   scoreVal.value = '';
+});
+
+// Sort events
+sort.addEventListener('change', (e) => {
+  if (e.target.value === '1') {
+    getScoresAscending();
+  } else if (e.target.value === '2') {
+    getScoresDescending();
+  } else if (e.target.value === '3') {
+    getNamesAscending();
+  } else if (e.target.value === '4') {
+    getNamesDescending();
+  }
 });
